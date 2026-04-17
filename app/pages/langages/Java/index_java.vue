ac7167d2004c5ac0f217dcf9e8d2f071271f127b
Thomas <thciccarelladev@gmail.com>
2026-04-17 12:35:05 +0200

    debut lessons fonction JAVA

--- app/pages/langages/Java/index_java.vue
+++ app/pages/langages/Java/index_java.vue
@@ -41,6 +41,22 @@
             </div>
         </div>
 
+        <!-- =============================================================\\F//==================================================================== -->
+        <div class="section-header">
+            <h3 class="lettreSommaire">F</h3>
+        </div>
+
+        <div class="card border-purple shadow-lg">
+            <div class="card-body bg-light-purple">
+                <h5 class="card-title text-purple">Fonction</h5>
+                <p class="card-text">Les fonction en JAVA et leur syntaxe</p>
+                <NuxtLink class="btn btn-purple btn-hover mt-auto text-white text-decoration-none"
+                    to="lessons/fonction">
+                    Accès à la leçon
+                </NuxtLink>
+            </div>
+        </div>
+
         <!-- =============================================================\\M//==================================================================== -->
         <div class="section-header">
             <h3 class="lettreSommaire">M</h3>
