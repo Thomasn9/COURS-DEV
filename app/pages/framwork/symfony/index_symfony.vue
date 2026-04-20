7691cf352a05fa5efcedea9fb46bcb62b373fb49
Thomas <thciccarelladev@gmail.com>
2026-04-20 13:55:51 +0200

    ajout lessons security.ymla

--- app/pages/framwork/symfony/index_symfony.vue
+++ app/pages/framwork/symfony/index_symfony.vue
@@ -289,6 +289,17 @@
 
         <div class="card border-purple shadow-lg">
             <div class="card-body bg-light-purple">
+                <h5 class="card-title text-purple">security.yaml</h5>
+                <p class="card-text">Le fichier security.ymal et son utiliter dans symfony</p>
+                <NuxtLink class="btn btn-purple btn-hover mt-auto text-white text-decoration-none"
+                    to="lessons/security_yaml">
+                    Accès à la leçon
+                </NuxtLink>
+            </div>
+        </div>
+
+        <div class="card border-purple shadow-lg">
+            <div class="card-body bg-light-purple">
                 <h5 class="card-title text-purple">Stimulus</h5>
                 <p class="card-text">Quest ce que stimulus et comment l'utiliser</p>
                 <NuxtLink class="btn btn-purple btn-hover mt-auto text-white text-decoration-none"
